e483d47c70e2af85e67e85e1b5730c2172930e87
OlhaMajerski <olha1991o@gmail.com>
2024-06-09 13:12:42 +0200

    timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,5 +1,15 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+// Описаний у документації
+import iziToast from 'izitoast';
+// Додатковий імпорт стилів
+import 'izitoast/dist/css/iziToast.min.css';
+
+const startBtn = document.querySelector('[data-start]');
+const clockface = document.querySelector('.value');
+let userSelectedDate;
+let intervalId = null;
+let initTime;
 const options = {
   enableTime: true,
   time_24hr: true,
@@ -12,6 +22,30 @@ const options = {
 
 flatpickr('input#datetime-picker', options);
 
-const startBtn = document.querySelector('[data-start]');
-let intervalId;
-let initTime;
+startBtn.addEventListener('click', () => {
+  intervalId = setInterval(() => {
+    const currentTime = Date.now();
+    const diff = initTime - currentTime;
+    const time = convertMs(diff);
+    const str = getTime(time);
+  });
+
+  function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // Remaining days
+    const days = Math.floor(ms / day);
+    // Remaining hours
+    const hours = Math.floor((ms % day) / hour);
+    // Remaining minutes
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+    return { days, hours, minutes, seconds };
+  }
+});
